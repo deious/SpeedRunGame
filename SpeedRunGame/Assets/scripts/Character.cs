8e926ac35bb94b0fc7f5ab745d51b0380b00b769
deious <gkstjddlekd@gmail.com>
2022-01-22 04:41:00 +0900

    이동 및 점프 추가

---
+++ SpeedRunGame/Assets/scripts/Character.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    // Start is called before the first frame update
+    const float moveSpeed = 3.0f;
+    const float jumpPower = 8.0f;
+    private bool jumpCheck = false;
+    Rigidbody2D rg;
+    void Start()
+    {
+        rg = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        if (Input.GetButtonDown("Jump"))
+        {
+            jumpCheck = true;
+            Jump();
+        }
+    }
+
+    void Move()
+    {
+        Vector3 moveVelocity = Vector3.zero;
+
+        if (Input.GetAxisRaw("Horizontal") < 0)
+            moveVelocity = Vector3.left;
+        else if (Input.GetAxisRaw("Horizontal") > 0)
+            moveVelocity = Vector3.right;
+
+        transform.position += moveVelocity * moveSpeed * Time.deltaTime;
+    }
+
+    void Jump()
+    {
+        if (!jumpCheck)
+            return;
+
+        rg.velocity = Vector2.zero;
+
+        Vector2 jumpVelocity = new Vector2(0, jumpPower);
+        rg.AddForce(jumpVelocity, ForceMode2D.Impulse);
+
+        jumpCheck = false;
+    }
+}
