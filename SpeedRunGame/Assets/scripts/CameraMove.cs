8d49290c4ea6306a8453569840ea5ec40eb73b21
deious <gkstjddlekd@gmail.com>
2022-01-26 22:32:52 +0900

    맵 수정 및 카메라 이동 추가

---
+++ SpeedRunGame/Assets/scripts/CameraMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public float cameraSpeed = 5.0f;
+
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Vector3 dir = player.transform.position - this.transform.position;
+        Vector3 moveVector = new Vector3(dir.x * cameraSpeed * Time.deltaTime, dir.y * cameraSpeed * Time.deltaTime, 0.0f);
+        this.transform.Translate(moveVector);
+    }
+}
